24368b8dac2e7ebb8e035cc9490007e89183469d
Jason Tempestini <jason@curlymedia.com>
2015-07-22 10:48:12 -0700

    Add helper transform, change name of contained folder

--- src/View/Helper/MarkdownHelper.php
+++ src/View/Helper/MarkdownHelper.php
@@ -7,11 +7,27 @@
 namespace Cake3xMarkdown\View\Helper;
 
 use Cake\View\Helper;
+use Cake3xMarkdown\Vendor\phpMarkdown\Michelf\Markdown;
 
 /**
  * CakePHP MarkdownHelper
  * @author jasont
  */
 class MarkdownHelper extends Helper {
-	//Here's a comment
+/**
+ * Convert markdown to html
+ *
+ * @param  string $text Text in markdown format
+ * @return string
+ */
+	public function transform($text) {
+		if (!isset($this->Parser)) {
+			if (!class_exists('Markdown')) {
+				App::import('Vendor', 'Markdown.MarkdownExtra' . DS . 'Michelf' . DS . 'Markdown');
+			}
+			$this->Parser = new Markdown();
+		}
+		return $this->Parser->transform($text);
+	}
+
 }
