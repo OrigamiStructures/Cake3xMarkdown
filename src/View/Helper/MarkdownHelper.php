4b2579894e8e71f49b3b789ffdf349a0bb0cf9fc
Jason Tempestini <jason@curlymedia.com>
2015-07-22 11:34:26 -0700

    Changes in loading process for markdown helper

--- src/View/Helper/MarkdownHelper.php
+++ src/View/Helper/MarkdownHelper.php
@@ -7,24 +7,27 @@
 namespace Cake3xMarkdown\View\Helper;
 
 use Cake\View\Helper;
-use Cake3xMarkdown\Vendor\phpMarkdown\Michelf\Markdown;
+use Cake3xMarkdown\Vendor\PhpMarkdown\Michelf\Markdown;
 
 /**
  * CakePHP MarkdownHelper
  * @author jasont
  */
 class MarkdownHelper extends Helper {
-/**
- * Convert markdown to html
- *
- * @param  string $text Text in markdown format
- * @return string
- */
+	
+	protected $Parser = NULL;
+	
+	/**
+	 * Convert markdown to html
+	 *
+	 * @param  string $text Text in markdown format
+	 * @return string
+	 */
 	public function transform($text) {
-		if (!isset($this->Parser)) {
-			if (!class_exists('Markdown')) {
-				App::import('Vendor', 'Markdown.MarkdownExtra' . DS . 'Michelf' . DS . 'Markdown');
-			}
+		if (is_null($this->Parser)) {
+//			if (!class_exists('Markdown')) {
+//				App::import('Vendor', 'Markdown.MarkdownExtra' . DS . 'Michelf' . DS . 'Markdown');
+//			}
 			$this->Parser = new Markdown();
 		}
 		return $this->Parser->transform($text);
