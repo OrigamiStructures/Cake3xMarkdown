740ea3a21f99b548e15ac4eb525f40aee51559c2
Jason Tempestini <jason@curlymedia.com>
2015-07-22 10:24:26 -0700

    Added MarkdownHelper file

---
+++ src/View/Helper/MarkdownHelper.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * Copyright 2015 Origami Structures
+ */
+
+namespace Cake3xMarkdown\View\Helper;
+
+use Cake\View\Helper;
+
+/**
+ * CakePHP MarkdownHelper
+ * @author jasont
+ */
+class MarkdownHelper extends Helper {
+	
+}
